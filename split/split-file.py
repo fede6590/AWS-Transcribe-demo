8d61b3432fa065f826484736d8466fc03e95edf1
Federico Ferreyra <federicoferreyra65@gmail.com>
2024-02-02 15:21:04 -0300

    local folder for ts-files.py develop

---
+++ split/split-file.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+
+def split_video(input_file, segment_duration):
+    base, _ = os.path.splitext(input_file)
+    output_manifest = f"{base}.m3u8"
+
+    command = f"ffmpeg -i {input_file} -c:v libx264 -c:a aac -f hls -hls_time {segment_duration} -hls_list_size 0 -hls_segment_filename stream/%03d.ts {os.path.join('stream', output_manifest)}"
+
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("Segmentation successful!")
+    except subprocess.CalledProcessError as error:
+        print("Error during segmentation:", error)
+
+
+filename = "video.mp4"
+duration = 2
+
+split_video(filename, duration)
